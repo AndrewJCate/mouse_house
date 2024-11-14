9ae136b57813e0abb4e66a050befcc536bb29a3c
Andrew Cate <andrewjcate@gmail.com>
2024-11-14 11:16:30 -0800

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+tailwind.config = {
+    darkMode: 'class',
+    theme: {
+        extend: {
+            height: {
+                '18': '4.5rem',
+                '22': '5.5rem',
+                '26': '6.5rem',
+                '28': '7.5rem',
+            }
+        }
+    }
+}
