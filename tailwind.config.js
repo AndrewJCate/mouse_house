2eaeebffb60613d9af36faa873059310ba6bc516
Andrew Cate <andrewjcate@gmail.com>
2024-11-14 16:58:38 -0800

    Adds products

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,21 @@ tailwind.config = {
     darkMode: 'class',
     theme: {
         extend: {
+            screens: {
+                '2xs': '286px',
+            },
             height: {
                 '18': '4.5rem',
                 '22': '5.5rem',
                 '26': '6.5rem',
                 '28': '7.5rem',
+            },
+            colors: {
+                'light':'#e2e8f0',
+                'dark':'#171717',
+                'bg-red':'#b91c1c',
+                'border-light':'#404040',
+                'border-dark':'#d4d4d4',
             }
         }
     }
