648c3b7421b76ce982cef9fa191f657edae1f63e
Andrew Cate <andrewjcate@gmail.com>
2024-11-21 12:40:46 -0800

    Finalizes layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,12 +12,17 @@ tailwind.config = {
                 '28': '7.5rem',
             },
             colors: {
-                'light':'#e2e8f0',
-                'dark':'#171717',
-                'bg-red':'#b91c1c',
-                'border-light':'#404040',
-                'border-dark':'#d4d4d4',
-            }
+                'light': '#e2e8f0',
+                'dark': '#171717',
+                'bg-red': '#b91c1c',
+                'border-light': '#404040',
+                'border-dark': '#d4d4d4',
+                'card-bg-dark': '#262626',
+                // 'card-bg-light': '#d5dbe2',
+            },
+            // boxShadow: {
+            //     'shadow-light': '0 1px 3px 0 rgb(255 255 255 / 0.1), 0 1px 2px -1px rgb(255 255 255 / 0.1)',
+            // }
         }
     }
 }
